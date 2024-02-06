1feb0b90fc8081a3ee837d21344d28ad39ffc832
TravisBell23 <149532586+TravisBell23@users.noreply.github.com>
2024-02-06 13:12:01 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onclick = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let noun = ["my dog", "a fish", "the tiger", "the cat"];
+  let verb = ["broke", "fixed", "smelled", "punched"];
+  let object = ["my homework", "the car", "the dishes"];
+  let when = ["before work", "after school", "during dinner"];
+
+  let randomnoun = Math.floor(Math.random() * noun.length);
+  let randomverb = Math.floor(Math.random() * verb.length);
+  let randomobject = Math.floor(Math.random() * object.length);
+  let randomwhen = Math.floor(Math.random() * when.length);
+
+  document.getElementById("excuse").innerHTML =
+    noun[randomnoun] +
+    " " +
+    verb[randomverb] +
+    " " +
+    object[randomobject] +
+    " " +
+    when[randomwhen];
 };
