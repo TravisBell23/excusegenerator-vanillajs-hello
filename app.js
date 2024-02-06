cfd5f03374bd7d69af1aca8dbe4ead275b7158e4
TravisBell23 <149532586+TravisBell23@users.noreply.github.com>
2024-02-06 01:53:15 +0000

    finished

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let c = 0; c < noun.length; c++) {
+console.log(pronoun[i], adj[j], noun[c])
+        }
+    }
+  }
+  
